0db88baa0ef5855476eefa2b43dfeed04e7ff9b8
Morgan Trotter <mhtrotte@ucsc.edu>
2025-01-26 20:10:08 -0800

    Changing home to reels

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/">Reels</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
